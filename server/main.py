4a77b289d2409395ab6e959aa9b8f0c1f3614dd9
iinaamasum <iinaamasum@gmail.com>
2022-11-24 16:05:27 +0600

    zip unzip while sending files to destination

--- server/main.py
+++ server/main.py
@@ -21,18 +21,30 @@ print(file_name, file_extension)
 if file_extension == "py":
     subprocess.call(f"python ./{file_name}.{file_extension} 1", shell=True)
 elif file_extension == "txt":
+    os.mkdir("TempDir")
     for i in range(1, 4):
         send_file_name = file_name + "_" + str(i) + "." + file_extension
 
         with open(f"./{file_name}.{file_extension}", "r") as file:
             lines = file.readlines()[0 : i * 10]
         file.close()
-        print(lines)
+        # shutil.copy(object_path, f"./files/{send_file_name}")
 
-        shutil.copy(object_path, send_file_name)
-        with open(send_file_name, "w") as file:
+        with open(f"TempDir/{send_file_name}", "w") as file:
             file.writelines([x for x in lines])
         file.close()
+
+    # zipping files
+    shutil.make_archive("send_files", "zip", "TempDir")
+    shutil.rmtree("./TempDir")
+
+    # sending files
+    zip_file = "./send_files.zip"
+    shutil.copy(zip_file, "../destination")
+
+    # unzipping in the destination
+    shutil.unpack_archive("../destination/send_files.zip", "../destination")
+
 
 else:
     print("The file is not either .txt or .py. So further process stopped here")
